2d4aa7a411480f73c955ac2c42d9423fb9f58ad5
ritchey0713 <ritchey0713@gmail.com>
2018-06-21 15:43:12 -0400

    built migration tables

--- db/migrate/20180621183230_create_rankings.rb
+++ db/migrate/20180621183230_create_rankings.rb
@@ -1,6 +1,8 @@
 class CreateRankings < ActiveRecord::Migration[5.2]
   def change
     create_table :rankings do |t|
+      t.belongs_to :user, foreign_key:  true
+      t.belongs_to :game, foreign_key: true 
 
       t.timestamps
     end
--- db/migrate/20180621183245_create_games.rb
+++ db/migrate/20180621183245_create_games.rb
@@ -1,6 +1,10 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
+      t.string :name
+      t.string :game_type
+      t.integer :play_time
+      t.string :main_setup 
 
       t.timestamps
     end
