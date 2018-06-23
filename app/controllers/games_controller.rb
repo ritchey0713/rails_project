9c513c21e588f2e28a337a575a8bc6cc75163969
ritchey0713 <ritchey0713@gmail.com>
2018-06-22 21:01:22 -0400

    added actions to game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,33 @@
 class GamesController < ApplicationController
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    if @game.update
+      redirect_to game_path(@game)
+    else
+      render :edit
+  end
+
+
 end
