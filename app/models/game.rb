7782fcad8798d38c32d8a267b03dc73f35251310
ritchey0713 <ritchey0713@gmail.com>
2018-06-24 13:28:59 -0400

    created helper method for game index

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,10 @@ class Game < ApplicationRecord
   has_many :rankings
   has_many :users, through: :rankings
   validates :name, presence: true
-  validates :play_time, numericality: true 
+  validates :play_time, numericality: true
+
+  def current_user_games(user)
+    Game.where("games.user_id = ?", user.id)
+  end
+  
 end
