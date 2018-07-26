eb0a7f9a3a2dae30bdca3f00386d44042be77491
ritchey0713 <ritchey0713@gmail.com>
2018-07-26 17:32:42 -0400

    add style to index

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -30,7 +30,7 @@ $(function(){
     $.getJSON(this.href,function(json){
       console.log(json)
       json.forEach(function(game){
-        $("div.gameName").append("Game: ",game["name"],"</br>", "Game time: ", game["play_time"],"</br>", "View Details...", "</br></br>")
+        $("div.gameName").append("<strong> Game: " ,game["name"],"</strong>", "</br>", "Game time: ", game["play_time"],"</br>", "View Details...", "</br></br>")
       })
     })
   })
