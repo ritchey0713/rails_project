e92fed53aed0fb16c154006cfea8bc874f516bd9
ritchey0713 <ritchey0713@gmail.com>
2018-07-26 21:12:16 -0400

    use handlebars for index

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.3.3'
 
 gem 'responders'
 
+gem 'handlebars_assets'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,3 +1,12 @@
+function Game(attr){
+this.name = attr.name
+this.game_type = attr.game_type
+this.play_time = attr.play_time
+this.main_setup = attr.main_setup
+this.score = attr.score
+}
+
+
 
 // getting the game edit form
 $(function(){
@@ -24,17 +33,49 @@ $(function(){
   })
 })
 // get json index
+// $(function(){
+//   $(".js-gameIndex").click(function(e){
+//     e.preventDefault()
+//     $.getJSON(this.href,function(json){
+//       console.log(json)
+//       json.forEach(function(game){
+//         $("div.gameName").append("<strong> Game: " ,game["name"],"</strong>", "</br>", "Game time: ", game["play_time"],"</br>", "View Details...", "</br></br>")
+//       })
+//     })
+//   })
+// })
+
+// use handlebars for json index
 $(function(){
-  $(".js-gameIndex").click(function(e){
-    e.preventDefault()
-    $.getJSON(this.href,function(json){
-      console.log(json)
-      json.forEach(function(game){
-        $("div.gameName").append("<strong> Game: " ,game["name"],"</strong>", "</br>", "Game time: ", game["play_time"],"</br>", "View Details...", "</br></br>")
+   $(".js-gameIndex").click(function(e){
+     this.user_id = $("h1").data("user-id")
+     e.preventDefault()
+     $.getJSON(this.href,function(games_array){
+      games_array.forEach(function(game){
+        var new_game = new Game(game)
+        var gameLi = new_game.renderLI()
+        $("ul#games_list").append(gameLi)
       })
     })
-  })
-})
+   })
+ })
+
+ $(function(){
+Game.templateSource = $("#games-template").html();
+Game.template = Handlebars.compile(Game.templateSource);
+ })
+
+Game.prototype.renderLI = function(){
+
+  return Game.template(this)
+}
+
+
+
+
+
+
+
 
 // show individual game
 $(function(){
