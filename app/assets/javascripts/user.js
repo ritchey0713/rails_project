b1fe402c361b93ecc1e13224cbd15f3e22983279
ritchey0713 <ritchey0713@gmail.com>
2018-07-27 18:01:04 -0400

    structure new game for HB

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,3 +1,4 @@
+//constructor prototype
 function Game(attr, user_id){
 this.id = attr.id
 this.name = attr.name
@@ -7,6 +8,93 @@ this.main_setup = attr.main_setup
 this.score = attr.score
 this.user_id = user_id
 }
+
+$(function(){
+Game.templateSource = $("#games-template").innerHTML;
+Game.template = Handlebars.compile($("#games-template").html());
+})
+
+Game.prototype.renderLI = function(){
+  return Game.template(this)
+}
+
+$(function(){
+  $("a.js-addGame").one("click",function(e){
+    e.preventDefault()
+    $.get(this.href).success(function(resp){
+      $("div.ajaxForm").html(resp)
+    }).error(function(){
+      window.location.replace("Logged_in_home")
+    })
+  })
+})
+
+//submit the new form via json
+$(function(){
+  $("form#new_game").submit(function(e){
+    var action = this.action
+    var params = $(this).serialize()
+    e.preventDefault()
+      $.ajax({
+        url: action,
+        data: params,
+        dataType: "json",
+        method: "POST"
+      })
+      .success(function(json){
+        var user_id = $("h1").data("user-id")
+        var game = new Game(json, user_id)
+        gameLI = game.renderLI()
+        $("div.ajaxForm").append(gameLI)
+        }).error(function(resp){
+        console.log("you got an error??", resp)
+      })
+      //function(res){
+        //$("div.ajaxForm").html(res)
+      //})
+  })
+})
+
+// handlebars template for index of games
+$(function(){
+   $(".js-gameIndex").click(function(e){
+
+     e.preventDefault()
+     $.getJSON(this.href,function(games_array){
+         //var user_id = $("h1").data("user-id")
+      games_array.forEach(function(game){
+        var user_id = $("h1").data("user-id")
+        var new_game = new Game(game, user_id)
+        var gameLi = new_game.renderLI()
+
+        //debugger
+        $("ul.games_list").append(gameLi)
+
+      })
+    })
+   })
+ })
+
+ // game details
+ $(function() {
+   $(document).on('click', 'a#showGame', function(e) {
+     e.preventDefault()
+     $.getJSON(this.href, function(game){
+       // event to remove index
+       // $("ul.games_list").remove()
+       $(".showHeading").html(`Full details`)
+       $(".gameName").text(`Game: ${game["name"]}`)
+       $(".gameType").text(`Game Type: ${game["game_type"]}`)
+       $(".gameTime").text(`Game Time: ${game["play_time"]}`)
+       $(".gameSetup").text(`Gear Setup: ${game["main_setup"]}`)
+       $(".gameScore").text(`Ranking Score: ${game["score"]}`)
+
+     })
+   })
+ })
+
+
+
 
 // getting the game edit form
 // $(function(){
@@ -32,89 +120,3 @@ this.user_id = user_id
 //       })
 //   })
 // })
-
-
-
-
-$(function(){
-  $("a.js-addGame").one("click",function(e){
-    e.preventDefault()
-    $.get(this.href).success(function(resp){
-      $("div.ajaxForm").html(resp)
-    }).error(function(){
-      window.location.replace("Logged_in_home")
-    })
-  })
-})
-
-$(function(){
-  $("form#new_game").submit(function(e){
-    var action = this.action
-    var params = $(this).serialize()
-    e.preventDefault()
-      $.ajax({
-        url: action,
-        data: params,
-        dataType: "json",
-        method: "POST"
-      })
-      .success(function(json){
-        console.log(json)
-      }).error(function(resp){
-        console.log("you got an error??", resp)
-      })
-      //function(res){
-        //$("div.ajaxForm").html(res)
-      //})
-  })
-})
-
-Game.prototype.user_id = $(function(){
-  $("h1").data("user-id")
-})
-// use handlebars for json index
-$(function(){
-   $(".js-gameIndex").click(function(e){
-
-     e.preventDefault()
-     $.getJSON(this.href,function(games_array){
-         //var user_id = $("h1").data("user-id")
-      games_array.forEach(function(game){
-        var user_id = $("h1").data("user-id")
-        var new_game = new Game(game, user_id)
-        var gameLi = new_game.renderLI()
-
-        //debugger
-        $("ul.games_list").append(gameLi)
-
-      })
-    })
-   })
- })
-
- $(function(){
-Game.templateSource = $("#games-template").innerHTML;
-Game.template = Handlebars.compile($("#games-template").html());
- })
-
-Game.prototype.renderLI = function(){
-  return Game.template(this)
-}
-
-// game details
-$(function() {
-  $(document).on('click', 'a#showGame', function(e) {
-    e.preventDefault()
-    $.getJSON(this.href, function(game){
-      // event to remove index
-      // $("ul.games_list").remove()
-      $(".showHeading").html(`Full details`)
-      $(".gameName").text(`Game: ${game["name"]}`)
-      $(".gameType").text(`Game Type: ${game["game_type"]}`)
-      $(".gameTime").text(`Game Time: ${game["play_time"]}`)
-      $(".gameSetup").text(`Gear Setup: ${game["main_setup"]}`)
-      $(".gameScore").text(`Ranking Score: ${game["score"]}`)
-
-    })
-  })
-})
