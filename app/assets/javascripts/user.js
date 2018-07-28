ac0cba7d266501210e61f647d0592c6983b40cfe
ritchey0713 <ritchey0713@gmail.com>
2018-07-27 20:33:37 -0400

    fix indents

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,37 +1,38 @@
 //constructor prototype
 function Game(attr, user_id){
-this.id = attr.id
-this.name = attr.name
-this.game_type = attr.game_type
-this.play_time = attr.play_time
-this.main_setup = attr.main_setup
-this.score = attr.score
-this.user_id = user_id
+    this.id = attr.id
+    this.name = attr.name
+    this.game_type = attr.game_type
+    this.play_time = attr.play_time
+    this.main_setup = attr.main_setup
+    this.score = attr.score
+    this.user_id = user_id
 }
 
 $(function(){
-Game.templateSource = $("#games-template").innerHTML;
-Game.template = Handlebars.compile($("#games-template").html());
+    Game.templateSource = $("#games-template").innerHTML;
+    Game.template = Handlebars.compile($("#games-template").html());
 })
 
 $(function(){
-Game.editSource = $("#edit-game-template").innerHTML;
-Game.editTemplate = Handlebars.compile($("#edit-game-template").html());
+    Game.editSource = $("#edit-game-template").innerHTML;
+    Game.editTemplate = Handlebars.compile($("#edit-game-template").html());
 })
 
 Game.prototype.renderLI = function(){
-  return Game.template(this)
+    return Game.template(this)
 }
 
 $(function(){
-  $("a.js-addGame").one("click",function(e){
-    e.preventDefault()
-    $.get(this.href).success(function(resp){
-      $("ul.games_list").html(resp)
-    }).error(function(){
-      window.location.replace("Logged_in_home")
+    $("a.js-addGame").one("click",function(e){
+      e.preventDefault()
+      $.get(this.href).success(function(resp){
+        $("ul.games_list").html(resp)
+        })
+      .error(function(){
+        window.location.replace("Logged_in_home")
+      })
     })
-  })
 })
 
 //submit the new form via json
@@ -40,23 +41,21 @@ $(function(){
     var action = this.action
     var params = $(this).serialize()
     e.preventDefault()
-      $.ajax({
-        url: action,
-        data: params,
-        dataType: "json",
-        method: "POST"
+    $.ajax({
+      url: action,
+      data: params,
+      dataType: "json",
+      method: "POST"
       })
       .success(function(json){
         var user_id = $("h1").data("user-id")
         var game = new Game(json, user_id)
         gameLI = game.renderLI()
         $("ul.games_list").append(gameLI)
-        }).error(function(resp){
-        console.log("you got an error??", resp)
       })
-      //function(res){
-        //$("div.ajaxForm").html(res)
-      //})
+        .error(function(resp){
+          console.log("you got an error??", resp)
+        })
   })
 })
 
@@ -119,20 +118,3 @@ $(function(){
         })
       })
   })
-
-
-
-
-
-
-//edit form submission
-// $(function(){
-//   $("form").submit(function(e){
-//     var url= this.action
-//     var data = $(this).serialize()
-//     e.preventDefault()
-//       $.post(url, data, function(res){
-//         $("div.ajaxForm").html(res)
-//       })
-//   })
-// })
